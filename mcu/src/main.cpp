102e5d566a2b09a44a90de46ecf0556c57540272
Adam K Dean <adamkdean@googlemail.com>
2020-04-16 21:52:34 +0100

    add connection status string

--- mcu/src/main.cpp
+++ mcu/src/main.cpp
@@ -15,7 +15,6 @@
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("Starting in 5s...");
 
   // Initial delay so I can connect to
   // serial and start monitoring.
@@ -34,7 +33,9 @@ void setup()
 void loop()
 {
   char msg[64];
-  sprintf(msg, "Status: %d", WiFi.status());
+  sprintf(msg, "Status: %s", wl_status_to_string(WiFi.status()));
+  Serial.println(msg);
+
   Serial.println(msg);
   digitalWrite(LED_BUILTIN, LOW);
   delay(100);
@@ -59,7 +60,8 @@ void connect_to_wifi() {
     delay(500);
   }
 
-  Serial.println("WiFi connected with IP " + WiFi.localIP());
+  sprintf(msg, "WiFi connected with IP %s", WiFi.localIP().toString().c_str());
+  Serial.println(msg);
 }
 
 //
@@ -72,5 +74,21 @@ void blink_excitedly(int count) {
     delay(50);
     digitalWrite(LED_BUILTIN, HIGH);
     delay(50);
+  }
+}
+
+//
+// WiFi status helper.
+//
+const char* wl_status_to_string(wl_status_t status) {
+  switch (status) {
+    case WL_NO_SHIELD: return "WL_NO_SHIELD";
+    case WL_IDLE_STATUS: return "WL_IDLE_STATUS";
+    case WL_NO_SSID_AVAIL: return "WL_NO_SSID_AVAIL";
+    case WL_SCAN_COMPLETED: return "WL_SCAN_COMPLETED";
+    case WL_CONNECTED: return "WL_CONNECTED";
+    case WL_CONNECT_FAILED: return "WL_CONNECT_FAILED";
+    case WL_CONNECTION_LOST: return "WL_CONNECTION_LOST";
+    case WL_DISCONNECTED: return "WL_DISCONNECTED";
   }
 }
--- mcu/src/main.h
+++ mcu/src/main.h
@@ -5,4 +5,5 @@
 // (C) Copyright 2020 Adam K Dean
 
 void connect_to_wifi();
-void blink_excitedly(int);
+void blink_excitedly(int count);
+const char* wl_status_to_string(wl_status_t status);
