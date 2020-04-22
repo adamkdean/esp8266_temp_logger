6d643f98a60cde72a3c7f3aa27eca811161319c3
Adam K Dean <adamkdean@googlemail.com>
2020-04-22 19:46:54 +0100

    remove old WiFiCredentials.h

--- mcu/include/WiFiCredentials.h
+++
@@ -1,8 +0,0 @@
-//  __ __  __   __  __   __   __
-// |_ (__ |__) (__)  _) /__  /__
-// |__ __)|    (__) /__ \__) \__)
-//
-// (C) Copyright 2020 Adam K Dean
-
-#define WIFI_SSID "ssidhere"
-#define WIFI_PASSWORD "passwordhere"
